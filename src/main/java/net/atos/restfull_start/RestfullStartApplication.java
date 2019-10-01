71fba8ce648433c5f9a33fb4419d85990e3e0112
MikiKru <michal_kruczkowski@o2.pl>
2019-10-01 09:04:14 +0200

    d2 v0

---
+++ src/main/java/net/atos/restfull_start/RestfullStartApplication.java
@@ -0,0 +1,13 @@
+package net.atos.restfull_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/atos/restfull_start/RestfullStartApplicationTests.java
@@ -0,0 +1,16 @@
+package net.atos.restfull_start;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfullStartApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
